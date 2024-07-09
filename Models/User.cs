c7dc20094b78fe28b31a6f7468c2f582efb6869f
Nours99 <77724655+Nours99@users.noreply.github.com>
2024-07-09 10:15:40 +0200

    Ajout classes User & Drawer

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string UserName { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required DateOnly Birthday { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+
+    }
+}
