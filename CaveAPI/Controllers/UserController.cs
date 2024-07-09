3d372847f5dc8ddbc56b8a52776f7c7f9802261d
perle <perle@HINAMI>
2024-07-09 16:52:57 +0200

    créeation controller, repository et interface

---
+++ CaveAPI/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Dal;
+using Dal.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CaveAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        [HttpGet]
+        public ActionResult Index() 
+        {
+            var users = context.Users.ToList(); // REPOSITORY
+            return Ok(users);
+        }
+    }
+}
---
+++ Dal/Repository/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal.Repository
+{
+    public class UserRepository
+    {
+        private readonly CellarContext _cellarContext;
+        public UserRepository(CellarContext cellarContext) 
+        {
+            _cellarContext = cellarContext;
+        }
+
+    }
+}
