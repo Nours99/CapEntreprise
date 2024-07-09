873405efa146f10ec9e257001a9f102ef493b69d
Nours99 <77724655+Nours99@users.noreply.github.com>
2024-07-09 11:53:14 +0200

    Ajout du context

---
+++ Dal/CellarContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Models;
+
+namespace Dal
+{
+    public class CellarContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Cellar> Cellars { get; set; }
+        public DbSet<Drawer> Drawers { get; set; }
+        public DbSet<Bottle> Bottles { get; set; }
+
+        public CellarContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected CellarContext()
+        {
+        }
+    }
+}
